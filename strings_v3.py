cca020bbb704e6db652ae2885a636d1fa5c3bc04
SpencercoFusion <spencertracks3141@gmail.com>
2020-02-15 17:39:17 -0500

    created strings v3

--- strings_v3.py
+++ strings_v3.py
@@ -60,8 +60,7 @@ def setToDimensions(img_to_resize, dimensions): #cuts and resizes  GREY image to
         #print(str(len(width_buffer)) + " is the height of buffer")
         #print(str(len(width_buffer[0])) + " is the length of buffer")
         resized_image = np.concatenate((width_buffer, resized_image, width_buffer), axis=1) 
-    return(resized_image)    
-
+    return(resized_image)
 
 def defineStringsAndConnectPoints(number_of_points, image, color_by_value):    #makes definitions for all strings connecting all points, and draws strings
     global list_of_string_definitions
@@ -109,93 +108,6 @@ def distance(p1, p2):                       #absolute distance between points p1
     distance = m.sqrt(  (x1-x2)**2  +   (y1-y2)**2  )
     return(distance)
 
-def valueOfLine(line_m, line_b, domain, image, string_definition):       #Value of line = avg pixel value * k
-    lower_x, upper_x = domain
-    if lower_x > upper_x:
-        l = upper_x
-        u = lower_x
-        lower_x = l
-        upper_x = u
-        print("error with range")
-
-    if (lower_x - upper_x) != 0:
-        total_value = 0
-        for x_val in range(upper_x - lower_x):
-            pixel_x = lower_x + x_val
-            pixel_y = int(pixel_x * line_m + line_b)
-            if ((center_x - pixel_x)**2 + (center_y - pixel_y)**2) < radius**2:
-                if(pixel_x < len(image[0]) and pixel_y < len(image)):
-                    total_value += image[pixel_y, pixel_x]
-                else:
-                    pass
-                    #print("out of bounds of IMAGE! point is at " + str((pixel_x, pixel_y)) + ", which is out of bounds for the image with size = " + str((len(image[0]), len(image))))
-
-            else:
-                pass
-                #print("out of bounds of CIRCLE! point is at " + str((pixel_x, pixel_y)) + ", which is out of bounds for the circle with radius = " + str(radius))
-        average_value = total_value / (upper_x - lower_x)
-        pixels_above_average = 0
-        for x_val in range(upper_x - lower_x):
-            pixel_x = lower_x + x_val
-            pixel_y = int(pixel_x * line_m + line_b)
-            if ((center_x - pixel_x)**2 + (center_y - pixel_y)**2) < radius**2:
-                if(pixel_x < len(image[0]) and pixel_y < len(image)):
-                    this_pixel_value = image[pixel_y, pixel_x]
-                    #print("this pixel " + str((pixel_x, pixel_y)) + " has a value of " + str(this_pixel_value) + " and average is " + str(average_value))
-                    if this_pixel_value > average_value:
-                        pixels_above_average += 1
-        percent_above_average = int(pixels_above_average / (upper_x - lower_x) * 100)
-        #print("percent above average for string " + str(string_definition) + ": " + str(percent_above_average))
-        global threshold_percent
-        if percent_above_average < threshold_percent:
-            average_value = 0
-    else:
-        average_value = 0
-        total_value = 0
-
-
-    #print("for string " + str(string_definition) + ", range between " + str(lower_x) + " and " + str(upper_x) + " is " + str(upper_x - lower_x))
-    return(average_value, total_value)
-
-def assignValuesToStrings():
-    for string in list_of_string_definitions:
-        p1, p2 = string
-        domain = pixelRange(string)
-        m, b = stringToLine(string)
-        average_value, total_value = valueOfLine(m, b, domain, inverted, string)
-
-        array_of_string_values[p2, p1] = int(average_value)
-
-
-def pixelRange(string):                             #given a string, returns the range from lower to upper x that the string is in
-    center_x, center_y = center
-
-    p1, p2 = string
-    angle1 = p1 * 2 * m.pi / number_of_points
-    point1_x = int(m.cos(angle1) * radius + center_x)
-    point1_y = int(m.sin(angle1) * radius + center_y)
-    angle2 = p2 * 2 * m.pi / number_of_points
-    point2_x = int(m.cos(angle2) * radius + center_x)
-    point2_y = int(m.sin(angle1) * radius + center_y)
-    #print("range for string " + str(string) + " is " + str(point1_x) + ", " + str(point2_x))
-    if point1_x < point2_x:
-        return(point1_x, point2_x)
-        domain = (point1_x, point2_x)
-    else:
-        return(point2_x, point1_x)
-        domain = (point2_x, point1_x)
-
-def stringToLine(string):
-    x1, y1 = pointToCoord(string[0])
-    x2, y2 = pointToCoord(string[1])
-    if x1 != x2:
-        m = (y1 - y2) / (x1 - x2)
-        b = y1 - m * x1
-    else:
-        m = "vert"
-        b = x1
-    return(m, b)
-
 def cursor_event(event, x, y, flags, param):
     global mouse_x
     global mouse_y
@@ -203,46 +115,24 @@ def cursor_event(event, x, y, flags, param):
         mouse_x = x
         mouse_y = y
 
-def colorPixelsInaLine(line_m, line_b, domain, image, string_definition):
-    string_val = array_of_string_values[string_definition[1], string_definition[0]]
-    #print(string_val)
-    lower_x, upper_x = domain
-    if lower_x > upper_x:
-        l = upper_x
-        u = lower_x
-        lower_x = l
-        upper_x = u
-        print("error with range")
-
-    if (lower_x - upper_x) != 0:
-        for x_val in range(upper_x - lower_x):
-            pixel_x = lower_x + x_val
-            pixel_y = int(pixel_x * line_m + line_b)
-            if ((center_x - pixel_x)**2 + (center_y - pixel_y)**2) < radius**2:
-                if(pixel_x < len(image[0]) and pixel_y < len(image)):
-                    #print("pixel " + str((pixel_x, pixel_y)) + " is " + str(image[pixel_y, pixel_x]) + ". adding " + str(string_val))
-                    image[pixel_y, pixel_x] += int(string_val)
-    return(image)
-
-def colorAllPixels(image):
-    for string in list_of_string_definitions:
-        p1, p2 = string
-        domain = pixelRange(string)
-        m, b = stringToLine(string)
-        image = colorPixelsInaLine(m, b, domain, image, string)
-    return image
-
-
+def error(original_image, constructed):
+    difference = original_image - constructed
+    error = cv2.mean(difference)
+    return(error)
 ######################################
 ######################################
 ######################################
 ######################################
+
+
+
+
 threshold_percent = 55
 
 frame_height = 1000
 frame_width = 1000
 
-number_of_points = 40
+number_of_points = 100
 center = (frame_width/2, frame_height/2)
 center_x, center_y = center
 radius = 400
@@ -264,23 +154,23 @@ img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   #the gray imported image image
 list_of_string_definitions = [] #the list of string definitions, where each string is defined as two points it goes through
 array_of_string_values = np.zeros(((number_of_points + 1), (number_of_points + 1))) #this is a two dimensional array, where each (x, y) is a string definition, and the value at (x, y) is the value for that string
 
-blank_colored = np.zeros(shape=(frame_height, frame_width, 1), dtype=np.uint8)  #this is just a blank_colored black screen. Used for drawing strings on
-final_strings =  np.zeros(shape=(frame_height, frame_width, 3), dtype=np.uint8) 
+blank_colored = np.zeros(shape=(frame_height, frame_width, 3), dtype=np.uint8)  #this is just a blank_colored black screen. Used for drawing strings on
+final_strings =  np.zeros(shape=(frame_height, frame_width, 1), dtype=np.uint8) 
 
 
 
 resized = setToDimensions(img_g, (frame_width, frame_height))
 inverted = 255 - resized
 defineStringsAndConnectPoints(number_of_points, blank_colored, False)
-assignValuesToStrings()
 
 #defineStringsAndConnectPoints(number_of_points, final_strings, True)
-final_strings = colorAllPixels(final_strings)
 ######################################
 ######################################
 ######################################
 ######################################
 print(array_of_string_values)
+
+print(error(inverted, final_strings))
 while(1):
     cv2.setMouseCallback("final strings", cursor_event)
     cv2.setMouseCallback("strings", cursor_event)
